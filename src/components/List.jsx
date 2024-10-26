859c5e0c754500bf65b1b74a377887748d5e1a22
PriyanshuPS <priyanshupratap@gmail.com>
2024-10-26 05:32:55 +0530

    created edit functionality

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,27 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const List = (props) => {
+  const [showedit, setshowedit] = useState(false)
+  const [newtodo, setnewtodo] = useState("")
+
   const handleDelete=(e, uid) =>{
-    let index = props.todos.findIndex(item=>{
-      return item.id === uid
-    })
-    console.log(index)
+    // let index = props.todos.findIndex(item=>{
+    //   return item.id === uid
+    // })
+    // console.log(index)
     let newtodos = props.todos.filter(item=>{
       return item.id !== uid
     })
     props.settodos(newtodos)
   }
+
+  const handleEdit=(e, uid) =>{
+    setshowedit(!showedit)
+    props.settodos(
+      props.todos.map((item) =>
+        item.id === uid ? { ...item, todo: newtodo } : item
+      )
+    )
+  }
+
+  const handleChange=(e) =>{
+    setnewtodo(e.target.value)
+  }
   return (
     <div className='list'>
         <div className="data">
           <input type="checkbox" />
-          <p style={{margin:0}}>{props.title}</p>
-
+          
+          {!showedit? <p style={{margin:0}}>{props.title}</p>: ""}
+          {showedit? <input onChange={handleChange} type="text" value= {newtodo} />: ""}
+          
         </div>
 
         <div className="option">
-            {/* <p>...</p> */}
-            <button>Edit</button>
+
+            <button onClick={(e) => handleEdit(e,props.id)}>{showedit?"Save": "Edit"}</button>
             <button onClick={(e) => handleDelete(e,props.id)}>Delete</button>
         </div>
     </div>
